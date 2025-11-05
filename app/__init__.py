6ad71abaf2f2deecabbeebf7f06f7e4232f968ce
Furkan <118442006+FurkanDagli@users.noreply.github.com>
2025-11-05 19:18:07 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -12,28 +12,3 @@ from app import routes, models
 
 
 
-"""
-from app import app,db
-from app.models import User, Post, Comment
-import sqlalchemy as sa
-app.app_context().push()
-u = User(username = 'Ahmet', email='ahmet@mail.com')
->>> db.session.commit()
->>> db.session.add(u)
->>> db.session.commit()
->>> u = User(username = 'Kagan', email='kagan@mail.com')
->>> db.session.commit()
->>> query = sa.select(User)
->>> users = db.session.scalars(query).all()
->>> users
->>> u = db.session.get(User, 1)
->>> p = Post(body='my first post', author=u)
->>> db.session.add(p)
->>> db.session.commit()
->>> query=u.posts.select()
->>> posts = db.session.scalars(query).all()
->>> posts
-
-
-
-"""
